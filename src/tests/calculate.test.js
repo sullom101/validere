6a940c3e31a00171e57fc7abdaf7208930397d50
Suleiman Mayow <mayow.sullom@gmail.com>
2021-07-29 01:22:48 -0400

    add calculate test and logic

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -3,9 +3,9 @@ describe("Calculate", () => {
   let expression;
   it("evaluates the expression correctly", () => {
     expression = "2+3+4-4*3";
-    expect(calculate(expression)).toBe(-3);
+    expect(calculate(expression)).toBe(15);
 
-    expression = "0+3+4";
+    expression = "0+3.+4";
     expect(calculate(expression)).toBe(7);
 
     expression = "0-9";
@@ -13,5 +13,11 @@ describe("Calculate", () => {
 
     expression = "0.5+2.3";
     expect(calculate(expression)).toBe(2.8);
+
+    expression = "2+3+4-+4*3";
+    expect(calculate(expression)).toBe(39);
+
+    expression = "2^2";
+    expect(calculate(expression)).toBe(4);
   });
 });
--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -1,16 +1,39 @@
-const operations = [
-  { "^": (a, b) => Math.pow(parseFloat(a), parseFloat(b)) },
-  { "*": (a, b) => parseFloat(a) * parseFloat(b) },
-  { "/": (a, b) => parseFloat(a) / parseFloat(b) },
-  { "+": (a, b) => parseFloat(a) + parseFloat(b) },
-  { "-": (a, b) => parseFloat(a) - parseFloat(b) },
-];
-
-const calculate = (exp) => {
-  //getting expressions "3,+,20,/,5,/,13,*,18,"
-  //change string to array
-  const array = exp.split(",");
-  console.log(array);
+import { operators } from "./constants";
+const operations = {
+  "^": (a, b) => Math.pow(a, b),
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "√": (a, b) => Math.sqrt(a, b),
 };
+
+function calculate(exp) {
+  //getting expressions "3+20/5/13*18"
+  //change string to array
+  for (let i = 0; i < operators.length; i++) {
+    let rg = new RegExp("\\" + operators[i], "g");
+    exp = exp.replace(rg, "," + operators[i] + ",");
+  }
+  const array = exp.split(",");
+  let result = 0;
+  let tempop;
+  array.map((a, index) => {
+    if (operations[a]) {
+      tempop = a;
+    } else {
+      if (index === 0) {
+        result = Number(result) + Number(a);
+      } else {
+        if (tempop) {
+          result = operations[tempop](Number(result), Number(a));
+          tempop = undefined;
+        }
+      }
+    }
+    return result;
+  });
+  return result;
+}
 
 export default calculate;
