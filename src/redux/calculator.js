830d23a33471fbb0b2633297d5b264e65f8c923c
Suleiman Mayow <mayow.sullom@gmail.com>
2021-07-29 01:23:36 -0400

    use calculate in reducers

--- src/redux/calculator.js
+++ src/redux/calculator.js
@@ -1,20 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { default as calculateUtil } from "../utils/calculate";
 export const calculationSlice = createSlice({
   name: "calculation",
   initialState: {
-    calculation: "3+2+3",
+    calculation: "",
     result: 0,
     history: [],
+    clearScreen: false,
   },
   reducers: {
     calculate: (state, action) => {
-      state.calculation += action.payload;
+      const calculation = action.payload;
+      const result = calculateUtil(calculation);
+      return {
+        ...state,
+        calculation,
+        result,
+        clearScreen: false,
+      };
+    },
+    total: (state, action) => {
+      const calculation = action.payload;
+      const result = calculateUtil(calculation);
+      return {
+        ...state,
+        calculation,
+        result,
+        history: [...state.history, { calculation, result }],
+        clearScreen: true,
+      };
+    },
+    clear: (state) => {
+      return {
+        ...state,
+        calculation: "",
+        result: 0,
+        clearScreen: false,
+      };
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { calculate } = calculationSlice.actions;
+export const { calculate, clear, total } = calculationSlice.actions;
 
 export default calculationSlice.reducer;
